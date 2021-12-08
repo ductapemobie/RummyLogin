f2d208625dd98b69583cc36bd74f5651a5a508d1
Willis <willislarsonm@gmail.com>
2021-12-08 17:48:49 -0500

    game dao completed

--- src/daos/game-dao-impl.ts
+++ src/daos/game-dao-impl.ts
@@ -12,16 +12,34 @@ export default class GameDaoImpl implements GameDao{
     }
 
     async getGameById(gameId: Number): Promise<Game> {
-        throw new Error("Method not implemented.");
+        const sql:String = "select * from game where game_id = $1";
+        const values = [gameId];
+        const result = await dbClient.query(sql, values);
+        const game:Game = result.rows.map(g => new Game(g.game_id, g.room_name, g.pass_word, g.players, g.player_limit, g.inSession))[0];
+        if (result.rowCount === 0)throw new Error("Game does not exist")
+        return game;
     }
+
     async getGames(): Promise<Game[]> {
-        throw new Error("Method not implemented.");
+        const sql:String = "select * from game";
+        const result = await dbClient.query(sql);
+        const games:Array<Game> = result.rows.map(g => new Game(g.game_id, g.room_name, g.pass_word, g.players, g.player_limit, g.inSession));
+        return games;
     }
     async updateGame(game: Game): Promise<Game> {
-        throw new Error("Method not implemented.");
+        const sql:String = "update game set (room_name,pass_word,players,player_limit,in_session)=($1,$2,$3,$4,$5) where game_id = $6";
+        const values = [game.roomName, game.password, game.players, game.playerLimit, game.inSession, game.gameId];
+        const result = await dbClient.query(sql, values);
+        if (result.rowCount === 0)throw new Error("Game does not exist")
+        game = result.rows.map(g => new Game(g.game_id, g.room_name, g.pass_word, g.players, g.player_limit, g.inSession))[0];
+        return game;
     }
     async deleteGame(gameId: Number): Promise<Boolean> {
-        throw new Error("Method not implemented.");
+        const sql:String = "delete from game where game_id = $1";
+        const values = [gameId];
+        const result = await dbClient.query(sql, values);
+        if (result.rowCount === 0)throw new Error("Game does not exist")
+        return true;
     }
     
 }
--- tests/game-dao-tests.spec.ts
+++ tests/game-dao-tests.spec.ts
@@ -7,29 +7,61 @@ const gameDao:GameDao = new GameDaoImpl();
 const testId:String = (Math.random() + 1).toString(36).substring(7);
 
 test("Create Game", async ()=>{
-    let testGame:Game = new Game(0, 'hi', 'hi', 0, 0, false);
+    let testGame:Game = new Game(0, testId+'create_game_test', 'password', 0, 0, false);
     testGame = await gameDao.createGame(testGame);
     expect(testGame.gameId).not.toBe(0);
 });
 
-afterAll(async ()=>{
-    dbClient.end();
-});
-
-/*import { dbClient } from "../src/connection";
-import GameDao from "../src/daos/game-dao";
-import GameDaoImpl from "../src/daos/game-dao-impl";
-import { Game } from "../src/entities";
-
-const gameDao:GameDao = new GameDaoImpl();
-const testId:String = (Math.random() + 1).toString(36).substring(7);
-
-test("Create Game", async ()=>{
-    let testGame:Game = new Game(0, testId+'create_test_game', 'create_test_pass', 0, 2, false);
+test("Get game by id", async ()=> {
+    let testGame:Game = new Game(0, testId+'get_game_test', 'password', 0, 0, false);
     testGame = await gameDao.createGame(testGame);
-    expect(testGame.gameId).toBe(0);
+    const gameId = testGame.gameId;
+    expect(gameId).not.toBe(0);
+    const findGame = await gameDao.getGameById(gameId);
+    expect(findGame.roomName).toBe(testGame.roomName);
 });
+
+test("Get all games", async () =>{
+    const testAccounts:Array<Game> = [];
+    for (let i = 0; i < 3; i ++){
+        let tempGame:Game = new Game(0, testId+'get_all_test_user' + i, 'password', 0, 0, false);
+        tempGame = await gameDao.createGame(tempGame);
+        testAccounts.push(tempGame);
+        expect(testAccounts[i].gameId).not.toBe(0);
+    }
+    const allGames:Array<Game> = await gameDao.getGames();
+    for (let i = 0; i < 3; i ++){
+        expect(testAccounts[i].roomName).toBe(allGames.filter(a=>a.gameId===testAccounts[i].gameId)[0].roomName)
+    }
+});
+
+test("Update game", async() =>{
+    let testGame:Game = new Game(0, testId+'create_game_test', 'password', 0, 0, false);
+    testGame = await gameDao.createGame(testGame);
+    expect(testGame.gameId).not.toBe(0);
+    testGame.password = 'updated_password'
+    await gameDao.updateGame(testGame);
+    const updatedGame = await gameDao.getGameById(testGame.gameId);
+    expect(updatedGame.password).toBe(testGame.password);
+});
+
+test("Delete game", async() =>{
+    let testGame:Game = new Game(0, testId+'create_game_test', 'password', 0, 0, false);
+    testGame = await gameDao.createGame(testGame);
+    const gameId = testGame.gameId;
+    expect(gameId).not.toBe(0);
+    await gameDao.deleteGame(gameId);
+    try{
+        console.error(gameId);
+        const deletedGame = await gameDao.getGameById(gameId);
+        expect(1).toBe(0);
+    }catch(error){
+        console.log(error);
+        expect(error.message).toBe("Game does not exist");
+    }
+})
 
 afterAll(async ()=>{
     dbClient.end();
-});*/
+});
+
