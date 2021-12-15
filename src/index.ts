4eb11bc9848cafe2582832456d92980215b3ddb3
Willis <willislarsonm@gmail.com>
2021-12-15 17:30:34 -0500

    endpoints for game side of api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import { Account, RetGame } from './entities';
+import { Account, Game, RetGame } from './entities';
 import AccountLogic from './business-logic/account-logic';
 import AccountLogicImpl from './business-logic/account-logic-impl';
 import jwt from 'jsonwebtoken'
@@ -49,7 +49,7 @@ app.patch('/users/login', async (req, res) =>{
 
 app.patch('/users/updatepassword', async (req, res) => {
     const token:any = req.headers.jwt;
-    let accountId:Number = 0;
+    let accountId:number = 0;
     const oldPassword:String = req.body.oldPassword;
     const newPassword:String = req.body.newPassword;
     try{
@@ -71,7 +71,7 @@ app.patch('/users/updatepassword', async (req, res) => {
 
 app.patch('/users/delete', async (req, res) =>{
     const token:any = req.headers.jwt;
-    let accountId:Number = 0;
+    let accountId:number = 0;
     const password:String = req.body.password;
     try{
         const result:any = jwt.verify(token, SECRET_KEY);   
@@ -94,7 +94,7 @@ app.get('/users/token', async (req, res) => {
     const token:any = req.headers.jwt;
     try{
         const result:any = jwt.verify(token, SECRET_KEY);
-        const accountId:Number = result.accountId;
+        const accountId:number = result.accountId;
         const userAccount = await accountLogic.getAccountById(accountId);
         const retAccount = {
             accountId:userAccount.accountId,
@@ -110,7 +110,183 @@ app.get('/users/token', async (req, res) => {
 
 app.get('/games', async (req, res)=>{
     const games:Array<RetGame> = await gameLogic.getAllGames();
-    return games;
+    res.status(200);
+    res.send(games);
+})
+
+app.post('/games', async (req, res) =>{
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        let game:Game = new Game(0, body.roomName, body.password, 0, body.playerLimit, false, accountId);
+        game = await gameLogic.createGame(game);
+        res.status(201);
+        res.send();
+    }catch(error){
+        res.status(403);
+        res.send(error.message);
+    }
+})
+
+app.patch("/games/join", async (req, res) => {
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    const gameId = body.gameId;
+    const password = body.password;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        await accountLogic.joinGame(accountId, gameId, password);
+        res.status(200)
+        res.send("Game joined successfully")
+    }catch(error){
+        res.status(403);
+        res.send(error.message)
+    }
+})
+
+app.patch("/games/leave", async (req, res) => {
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        await accountLogic.leaveGame(accountId);
+        res.status(200)
+        res.send("Game left successfully")
+    }catch(error){
+        res.status(403);
+        res.send(error.message)
+    }
+})
+
+app.patch("/games/start", async (req, res) =>{
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    const gameId = body.gameId;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        await gameLogic.startGame(gameId, accountId);
+        res.status(200)
+        res.send("Game started successfully")
+    }catch(error){
+        res.status(401);
+        res.send(error.message);
+    }
+})
+
+app.patch("/games/stop", async (req, res) =>{
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    const gameId = body.gameId;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        await gameLogic.userStopGame(gameId, accountId);
+        res.status(200)
+        res.send("Game stopped successfully")
+    }catch(error){
+        res.status(401);
+        res.send(error.message);
+    }
+})
+
+app.patch("/games/delete", async (req, res) =>{
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    const gameId = body.gameId;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        await gameLogic.deleteGame(gameId, accountId);
+        res.status(200)
+        res.send("Game deleted successfully")
+    }catch(error){
+        res.status(401);
+        res.send(error.message);
+    }
+})
+
+app.patch("/games/harddelete", async (req, res) =>{
+    const token:any = req.headers.jwt;
+    const body = req.body;
+    const gameId = body.gameId;
+    let accountId:number = 0;
+    try{
+        const result:any = jwt.verify(token, SECRET_KEY);
+        accountId = result.accountId;
+    }catch(error){
+        res.status(401);
+        res.send(error.message)
+    }
+    try{
+        await gameLogic.hardDeleteGame(gameId, accountId);
+        res.status(200)
+        res.send("Game deleted successfully")
+    }catch(error){
+        res.status(401);
+        res.send(error.message);
+    }
+})
+
+app.get("/games/:gameId", async(req, res) => {//gets game details and assoc players
+    const gameId = parseInt(req.params.gameId);
+    try{
+        const game = await gameLogic.getGameById(gameId);
+        const playerList = await gameLogic.getAssocPlayers(gameId);
+        const retObj = {
+            roomName:game.roomName,
+            gameOwner:game.gameOwner,
+            players:game.players,
+            playerLimit:game.playerLimit,
+            inSession:game.inSession,
+            playerList:playerList
+        }
+        res.status(200);
+        res.send(retObj); 
+    }catch(error){
+        res.status(401);
+        res.send(error.message);
+    }
 })
 
 app.listen(PORT, ()=>{
